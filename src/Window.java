cb9e5666dd4078a63ffb6a18ef0551bd4412ef04
Pontus Asp <pontus.asp@gmail.com>
2020-01-20 17:33:11 +0100

    initial commit

---
+++ src/Window.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class Window {
+
+    public Window() {
+        JFrame window = new JFrame("Very cool graphics");
+        World world = new World();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
