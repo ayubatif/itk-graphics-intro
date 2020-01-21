29d384c99441aa935af5b5a9fc46b76ba844e8fe
Pontus Asp <pontus.asp@gmail.com>
2020-01-21 08:35:05 +0100

    added game loop

--- src/World.java
+++ src/World.java
@@ -15,15 +15,41 @@ public class World extends JPanel {
     }
 
     public void start() {
-        repaint();
+        running = true;
+
+        while(running) {
+            try {
+                float sleepTime = 1f/60 * 1000;
+                update(sleepTime / 1000);
+                repaint();
+                Thread.sleep((int) sleepTime);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
+    float x = 50;
+    float y = 50;
     @Override
     public void paintComponent(Graphics g) {
         g.setColor(Color.BLACK);
         g.fillRect(0, 0, WIDTH, HEIGHT);
 
-        repaint();
+        g.setColor(Color.CYAN);
+        g.fillRect((int) x, (int) y, 50, 50);
+    }
+
+    public void update(float delta) {
+        x += 200 * delta;
+        y += 200 * delta;
+
+        if(x > WIDTH) {
+            x = -50;
+        }
+        if(y > HEIGHT) {
+            y = -50;
+        }
     }
 
 }
