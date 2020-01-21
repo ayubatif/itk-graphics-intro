30aec1f7804d4840a3ff1f51e82e61e228f6c201
Pontus Asp <pontus.asp@gmail.com>
2020-01-21 08:23:40 +0100

    created window

--- src/Window.java
+++ src/Window.java
@@ -1,12 +1,23 @@
+import javax.swing.*;
 
 public class Window {
 
     public Window() {
+        JFrame window = new JFrame("Very cool ITK graphics");
+        World world = new World(800, 600);
 
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+
+        window.add(world);
+        window.pack();
+        window.setLocationRelativeTo(null);
+
+        window.setVisible(true);
     }
 
     public static void main(String[] args) {
-
+        Window window = new Window();
     }
 
 }
--- src/World.java
+++ src/World.java
@@ -1,3 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class World {
+public class World extends JPanel {
+
+    public final int WIDTH, HEIGHT;
+    public boolean running = false;
+
+    public World(int WIDTH, int HEIGHT) {
+        this.WIDTH = WIDTH;
+        this.HEIGHT = HEIGHT;
+
+        Dimension screenSize = new Dimension(WIDTH, HEIGHT);
+        setPreferredSize(screenSize);
+    }
+
 }
